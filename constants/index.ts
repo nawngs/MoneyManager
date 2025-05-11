37fb7aaea24a22a0c3dfd9631ce352de0157903f
Hung <duyhung6872982006@gmail.com>
2025-05-11 19:23:48 +1000

    cloudinary

---
+++ constants/index.ts
@@ -0,0 +1,2 @@
+export const CLOUDINARY_CLOUD_NAME = "dbnl3ya63";
+export const CLOUDINARY_UPLOAD_PRESET = "images";
--- services/userService.ts
+++ services/userService.ts
@@ -1,12 +1,20 @@
 import { firestore } from "@/config/firebase";
 import { ResponseType, UserDataType } from "@/types";
 import { doc, updateDoc } from "firebase/firestore";
+import { uploadFile } from "./imageService";
 
 export const updateUser = async (
   uid: string,
   updateData: UserDataType,
 ): Promise<ResponseType> =>{
   try {
+    if (updateData.image && updateData?.image?.uri) {
+      const imageUpload = await uploadFile(updateData.image, "users");
+      if (!imageUpload.success) {
+        return {success: false, msg: imageUpload.msg || 'Failed to upload image'};
+      }
+      updateData.image = imageUpload.data;
+    }
     const userRef = doc(firestore, "users", uid);
     await updateDoc(userRef, updateData);
     return {success: true};
